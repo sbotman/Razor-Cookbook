6200c4fb6908c10db7cfb17e623ca7a340f0b144
Sander Botman <sbotman@schubergphilis.com>
2013-07-15 01:31:43 +0200

    Update bootstrap.sh
    Using Chef-solo now, so this is not needed

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,8 +13,6 @@ rpm -q rubygems; if [ $? -ne 0 ]; then yum install rubygems -y; fi
 
 grep $HOSTNAME /etc/hosts; if [ $? -ne 0 ]; then echo "$(ifconfig eth0 | grep -Eo 'addr:[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | cut -d: -f2)    $HOSTNAME" >> /etc/hosts ; fi
 
-grep chef /etc/hosts; if [ $? -ne 0 ]; then echo "10.0.2.15   chef" >> /etc/hosts ; fi
-
 if [ ! -f /etc/yum.repos.d/10gen.repo  ]; then
 
 cat > /etc/yum.repos.d/10gen.repo  << "EOF"
